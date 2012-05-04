bb73eca63080a3f1496648f107e03b100039bb41
Aldrin Martoq <aldrin@martoq.cl>
2012-05-03 21:51:04 -0400

    Initial Commit

---
+++ brcore/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  brcore
+//
+//  Created by Aldrin Martoq on 5/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
